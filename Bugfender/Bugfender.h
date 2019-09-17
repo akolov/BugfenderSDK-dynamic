4d31bf2be8b83cc5849929f05551fed09a93f2bb
Alexander Kolov <me@alexkolov.com>
2019-09-17 13:22:10 +0900

    Wrap Bugfender SDK in a dynamic framework

---
+++ Bugfender/Bugfender.h
@@ -0,0 +1,22 @@
+//
+//  Bugfender.h
+//  Bugfender
+//
+//  Created by Alexander Kolov on 2019-09-17.
+//  Copyright © 2019 Alexander Kolov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Bugfender/BugfenderSDK.h>
+#import <Bugfender/BFUserFeedbackNavigationController.h>
+#import <Bugfender/BFUserFeedbackViewController.h>
+
+//! Project version number for Bugfender.
+FOUNDATION_EXPORT double BugfenderVersionNumber;
+
+//! Project version string for Bugfender.
+FOUNDATION_EXPORT const unsigned char BugfenderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bugfender/PublicHeader.h>
+
+
